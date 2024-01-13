210abac47e1a28b9d9f39570971047b311d75176
BrahianPuschiasis <brahian.puschiasis@gmail.com>
2024-01-13 17:18:26 -0300

    links fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex items-center justify-center">
         <div className="text-white">
-          <Link to="/curriculum" className="text-xl font-semibold hover:text-blue-400 hover:shadow-md transition duration-300 ease-in-out">Home</Link>
+          <Link to="/" className="text-xl font-semibold hover:text-blue-400 hover:shadow-md transition duration-300 ease-in-out">Home</Link>
         </div>
         <div className="text-white ml-4">
           <Link to="/projects" className="text-xl font-semibold hover:text-blue-400 hover:shadow-md transition duration-300 ease-in-out">Projects</Link>
