55c4e6285aa05b6305807817253cd540e7c8ebf9
BrahianPuschiasis <brahian.puschiasis@gmail.com>
2024-07-01 15:10:48 -0300

    modal for languages working with they flags

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,41 +1,47 @@
 /* eslint-disable no-unused-vars */
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLanguage } from "../context/LanguajeContext.jsx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
-import { useTheme } from '../context/ThemeContext.jsx';
+import { faSun, faMoon, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { useTheme } from "../context/ThemeContext.jsx";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
 
 const Navbar = () => {
   const { isDarkMode, toggleDarkMode } = useTheme();
   const { i18n } = useTranslation();
   const { toggleLanguage, language } = useLanguage();
   const { t } = useTranslation();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleToggleClick = () => {
-    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  document.documentElement.classList.add('dark')
-} else {
-  document.documentElement.classList.remove('dark')
-}
-
-// Whenever the user explicitly chooses light mode
-localStorage.theme = 'light'
-
-// Whenever the user explicitly chooses dark mode
-localStorage.theme = 'dark'
-
-// Whenever the user explicitly chooses to respect the OS preference
-localStorage.removeItem('theme')
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
     toggleDarkMode();
   };
 
-  const toggleLanguageHandler = () => {
-    const newLanguage = language === "en" ? "es" : "en";
+  const openLanguageModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeLanguageModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const changeLanguage = (newLanguage) => {
     i18n.changeLanguage(newLanguage);
     toggleLanguage(newLanguage);
+    closeLanguageModal();
   };
 
   useEffect(() => {}, [language]);
@@ -43,7 +49,7 @@ localStorage.removeItem('theme')
   return (
     <nav className="bg-blue-500 dark:bg-slate-900 p-2">
       <div className="container mx-auto flex items-center justify-center">
-        <div className="text-white ">
+        <div className="text-white">
           <Link
             to="/"
             className="text-xl font-semibold hover:text-blue-400 hover:shadow-md transition duration-300 ease-in-out"
@@ -61,23 +67,61 @@ localStorage.removeItem('theme')
         </div>
         <div className="ml-4">
           <button
-            onClick={toggleLanguageHandler}
+            onClick={openLanguageModal}
             className="text-white hover:text-blue-400 focus:outline-none"
           >
-            {language === "en" ? "ES" : "EN"}
+            {language.toUpperCase()}
           </button>
         </div>
         <div className="ml-4">
-          <button onClick={handleToggleClick} className="text-white hover:text-blue-400 focus:outline-none">
+          <button
+            onClick={handleToggleClick}
+            className="text-white hover:text-blue-400 focus:outline-none"
+          >
             {isDarkMode ? (
               <FontAwesomeIcon icon={faSun} className="text-xl" />
             ) : (
               <FontAwesomeIcon icon={faMoon} className="text-xl" />
             )}
           </button>
-          
         </div>
       </div>
+
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeLanguageModal}
+        contentLabel="Language Selection"
+        className="Modal"
+        overlayClassName="Overlay"
+      >
+        <div className="flex flex-col space-y-2 mt-4 icon-container">
+          <button
+            onClick={() => changeLanguage("es")}
+            className="flex items-center justify-start w-full px-4 py-2 text-gray-700 hover:bg-gray-100 hover:text-blue-400"
+          >
+            <img
+              src="src/images/espana.png"
+              alt="ES"
+              className="w-6 h-6 mr-2"
+            />
+            ES
+          </button>
+          <button
+            onClick={() => changeLanguage("en")}
+            className="flex items-center justify-start w-full px-4 py-2 text-gray-700 hover:bg-gray-100 hover:text-blue-400"
+          >
+            <img src="src/images/us.png" alt="EN" className="w-6 h-6 mr-2" />
+            EN
+          </button>
+          <button
+            onClick={() => changeLanguage("pt")}
+            className="flex items-center justify-start w-full px-4 py-2 text-gray-700 hover:bg-gray-100 hover:text-blue-400"
+          >
+            <img src="src/images/br.png" alt="PT" className="w-6 h-6 mr-2" />
+            PT
+          </button>
+        </div>
+      </Modal>
     </nav>
   );
 };
